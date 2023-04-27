0e273248d598753853feabf88fe4f456e4232c9c
yuragms <yura.gms@gmail.com>
2023-04-27 21:45:54 +0300

    login-panel

--- layout/layout.js
+++ layout/layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
     <div className="flex h-screen bg-blue-400">
       <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
         <div className={styles.imgStyle}>Images</div>
-        <div className="right flex flex-col justify-evenly bg-gray-500">
+        <div className="right flex flex-col justify-evenly">
           <div className="text-center py-10"> {children}</div>
         </div>
       </div>
--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import React from 'react';
 import Layout from '../layout/layout';
+import Link from 'next/link';
 
 export default function Login() {
   return (
@@ -8,7 +9,41 @@ export default function Login() {
       <Head>
         <title>Login</title>
       </Head>
-      <h1>Login</h1>
+      <section className="w-3/4 mx-auto flex flex-col gap-10">
+        <div className="title">
+          <h1 className="text-gray-800 text-4xl font-bold py-4">Explore</h1>
+          <p className="w-3/4 mx-auto text-gray-400">
+            Lorem ipsum dolor sit amet consectrtur adipisicing elit. Dolores,
+            officia
+          </p>
+        </div>
+        {/*form */}
+        <form className="flex flex-col gap-5">
+          <div className="input-group">
+            <input type="email" name="email" placeholder="Email" />
+          </div>
+          <div className="input-group">
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          {/*login button */}
+          <div className="input-button">
+            <button type="submit">Login</button>
+          </div>
+          <div className="input-button">
+            <button type="submit">Sign In With Google</button>
+          </div>
+          <div className="input-button">
+            <button type="submit">Sign In With GitHub</button>
+          </div>
+        </form>
+        {/*bottom */}
+        <p className="text-center text-gray-400">
+          don't have an account yet?
+          <Link href={'/register'} className="text-blue-700">
+            Sign Up
+          </Link>
+        </p>
+      </section>
     </Layout>
   );
 }
