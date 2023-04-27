34871badb843c74d2ec91929123d85db10fdec2b
yuragms <yura.gms@gmail.com>
2023-04-27 19:32:01 +0300

    login-register

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1>Login</h1>
+    </div>
+  );
+}
---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Register() {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <h1>Register</h1>
+    </div>
+  );
+}
