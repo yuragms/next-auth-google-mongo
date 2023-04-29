8df05ca2364a755bef6df5f6a3e58248400b8ba4
yuragms <yura.gms@gmail.com>
2023-04-29 13:04:22 +0300

    ProtectedRoute

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,15 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import { useState } from 'react';
-import { useSession } from 'next-auth/react';
+import { getSession, signOut, useSession } from 'next-auth/react';
 
 export default function Home() {
   // const [session, setSession] = useState(true);
   const { data: session } = useSession();
+
+  function handleSignOut() {
+    signOut();
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -15,7 +19,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {session ? User({ session }) : Guest()}
+      {session ? User({ session, handleSignOut }) : Guest()}
     </div>
   );
 }
@@ -38,7 +42,7 @@ function Guest() {
 }
 //Authorize
 
-function User({ session }) {
+function User({ session, handleSignOut }) {
   return (
     <main className="container mx-auto text-center py-20">
       <h3 className="text-4xl font-bold">Authorize User Homepage</h3>
@@ -47,7 +51,10 @@ function User({ session }) {
         <h5>{session.user.email}</h5>
       </div>
       <div className="flex justify-center">
-        <button className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50">
+        <button
+          onClick={handleSignOut}
+          className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50"
+        >
           Sign Out
         </button>
       </div>
@@ -61,4 +68,21 @@ function User({ session }) {
       </div>
     </main>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
