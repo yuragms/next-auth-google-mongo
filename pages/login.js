cc0667c409d9992998bcc3704eacd16207e4add1
yuragms <yura.gms@gmail.com>
2023-04-30 13:55:50 +0300

    loginValidate

--- pages/login.js
+++ pages/login.js
@@ -8,14 +8,18 @@ import { FaGithub } from 'react-icons/fa';
 import { HiAtSymbol, HiFingerPrint } from 'react-icons/hi';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { useFormik } from 'formik';
+import login_validate from '../lib/validate';
 
 export default function Login() {
   const [show, setShow] = useState(false);
+
+  //formik hook
   const formik = useFormik({
     initialValues: {
       email: '',
       password: '',
     },
+    validate: login_validate,
     onSubmit,
   });
 
@@ -60,6 +64,11 @@ export default function Login() {
               <HiAtSymbol size={25} />
             </span>
           </div>
+          {formik.errors.email && formik.touched.email ? (
+            <span className="text-rose-500">{formik.errors.email}</span>
+          ) : (
+            <></>
+          )}
           <div className={styles.input_group}>
             <input
               type={`${show ? 'text' : 'password'}`}
@@ -75,6 +84,11 @@ export default function Login() {
               <HiFingerPrint size={25} />
             </span>
           </div>
+          {formik.errors.password && formik.touched.password ? (
+            <span className="text-rose-500">{formik.errors.password}</span>
+          ) : (
+            <></>
+          )}
           {/*login button */}
           <div className="input-button">
             <button type="submit" className={styles.button}>
