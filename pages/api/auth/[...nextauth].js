d6735f4a41d98f3a7748ab14375bf0ec1b22945d
yuragms <yura.gms@gmail.com>
2023-05-06 15:22:26 +0300

    1

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import connectMongo from '../../../database/conn';
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -11,6 +13,10 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      async authorize(credentials, req) {},
     }),
     // ...add more providers here
   ],
